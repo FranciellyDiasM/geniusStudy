f2f55956b476bc1cb8823892732240b8b427c196
FranciellyDiasM <frandiasma@gmail.com>
2024-06-05 19:16:02 -0300

    initial commit

---
+++ src/main/java/br/com/dias/geniustudy/GeniuStudy.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.dias.geniustudy;
+
+/**
+ *
+ * @author frand
+ */
+public class GeniuStudy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
