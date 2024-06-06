c8598d1f248c2f193ce663c787ad5ccc092ceb77
FranciellyDiasM <frandiasma@gmail.com>
2024-06-05 21:21:09 -0300

    alteracao de pastas

--- src/main/java/br/com/dias/geniustudy/view/JFLogin.java
+++ src/main/java/br/com/dias/geniustudy/view/JFLogin.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package br.com.dias.geniustudy;
+package br.com.dias.geniustudy.view;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
